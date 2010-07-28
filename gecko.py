b88536ee4ca6bdf1a94df7fc94af36ade844ad4c
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2010-07-28 02:58:10 +0200

    Add the ::selected selector to gecko.

--- gecko.py
+++ gecko.py
@@ -12,9 +12,16 @@ from ._helpers import odict
 def transform(parser, keep_existant=True):
     for name, attributes in parser.items():
         section = odict()
+        force_keep_existant = False
+
+        # Parsing sections
+        if '::selection' in name:
+            parser[name] = {}
+            name = name.replace('::selection', '::-moz-selection')
+            force_keep_existant = True
 
         for attribute, value in attributes.items():
-            if keep_existant:
+            if keep_existant or force_keep_existant:
                 section[attribute] = value
 
             # Parsing attributes
