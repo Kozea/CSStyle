7cb75a6a0ec91c211b7b80eb008e62bb1a4b8978
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2011-08-11 10:49:46 +0200

    Add support for transformations for IE.

--- csstyle/trident.py
+++ csstyle/trident.py
@@ -5,3 +5,18 @@ CSStyle Trident Parser
 CSS transformer for Microsoft Internet Explorer
 
 """
+
+from ._helpers import OrderedDict
+
+
+def transform(parser, keep_existant=True):
+    """Add Trident-specific attributes to ``parser``."""
+    for name, attributes in parser.items():
+        section = OrderedDict()
+
+        for attribute, value in attributes.items():
+            if keep_existant:
+                section[attribute] = value
+
+            if attribute.startswith('transform'):
+                section['-ms-%s' % attribute] = value
